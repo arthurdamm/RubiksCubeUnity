c905fb1e343d6042b16a3420776e969f260d2b71
Arthur Damm <indigoarthur@gmail.com>
2026-05-10 16:36:02 -0700

    Partial rotation

--- Assets/Scripts/Runtime/CubeController.cs
+++ Assets/Scripts/Runtime/CubeController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CubeController : MonoBehaviour
 {
@@ -14,6 +15,8 @@ public class CubeController : MonoBehaviour
     {
         _cubieBounds = cubiePrefab.GetComponent<MeshFilter>().sharedMesh.bounds;
         SpawnCubies();
+        RotateLayer(CubeLayer.L, 90);
+        RotateLayer(CubeLayer.F, 45);
     }
 
     // Update is called once per frame
@@ -41,8 +44,63 @@ public class CubeController : MonoBehaviour
                     GameObject cubieGo = Instantiate(cubiePrefab, spawnPosition, Quaternion.identity, transform);
                     cubieGo.name = $"Cubie ({x}, {y}, {z})";
                     _cubies[x, y, z] = cubieGo.transform;
+                    
                 }
             }
         }
     }
+
+    private void RotateLayer(CubeLayer layer, float degrees)
+    {
+        int xStart = 0, xStop = _cubeSize - 1, yStart = 0, yStop = _cubeSize - 1, zStart = 0, zStop = _cubeSize - 1;
+        
+        switch (layer)
+        {
+            case CubeLayer.L: xStart = xStop = 0; break;
+            case CubeLayer.X: xStart = xStop = 1; break;
+            case CubeLayer.R: xStart = xStop = 2; break;
+            case CubeLayer.D: yStart = yStop = 0; break;
+            case CubeLayer.Y: yStart = yStop = 1; break;
+            case CubeLayer.U: yStart = yStop = 2; break;
+            case CubeLayer.F: zStart = zStop = 0; break;
+            case CubeLayer.Z: zStart = zStop = 1; break;
+            case CubeLayer.B: zStart = zStop = 2; break;
+        }
+
+        Vector3 center = _cubies[(xStart + xStop) / 2, (yStart + yStop) / 2, (zStart + zStop) / 2].position;
+        Vector3 axis = Vector3.up;
+
+        switch ((int)layer / _cubeSize)
+        {
+            case 0 : axis = Vector3.right; break;
+            case 1 : axis = Vector3.up; break;
+            case 2 : axis = Vector3.forward; break;
+        }
+        
+
+        for (int x = xStart; x <= xStop; x++)
+        {
+            for (int y = yStart; y <= yStop; y++)
+            {
+                for (int z = zStart; z <= zStop; z++)
+                {
+                    _cubies[x, y, z].RotateAround(center, axis, degrees);
+                }
+            }
+        }
+        
+    }
+}
+
+enum CubeLayer
+{
+    L,
+    X,
+    R,
+    D,
+    Y,
+    U,
+    F,
+    Z,
+    B
 }
